f331ce0aaaac3e214be0c386ce33221fec28c8a3
suguru-3u <suguru.13579515@gmail.com>
2024-02-17 14:51:19 +0900

    Perform vue tutorials 9 to 15

---
+++ src/components/ChildComp.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+// propsの受け取りを宣言
+const props = defineProps({
+  msg: String
+})
+
+// 発行されるイベントを宣言します
+const emit = defineEmits(['response'])
+// 引数つきで発行
+emit('response', 'hello from child')
+</script>
+
+<template>
+  <div>
+    <h1>{{ props.msg }}</h1>
+    <br />
+    <br />
+    <slot>Fallback content</slot>
+    <br />
+    <br />
+  </div>
+</template>
+
+<style></style>
