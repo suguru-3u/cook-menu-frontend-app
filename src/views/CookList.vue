7772336135d40f8957eb2cbf966fc78b61a46496
suguru-3u <suguru.13579515@gmail.com>
2024-02-18 20:30:23 +0900

    Started creating a demo site

---
+++ src/views/CookList.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <H1>This is Cook menu list</H1>
+    <ul>
+      <li>親子丼</li>
+      <li>そば</li>
+      <li>鯖の味噌煮</li>
+      <li>豚汁</li>
+      <li>生姜焼き</li>
+    </ul>
+  </div>
+</template>
